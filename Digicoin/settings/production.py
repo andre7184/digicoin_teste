d856903e222dcf2c0ce7731999fc6da869a53497
Andre Martins <andre53793016@aluno.ms.senac.br>
2025-10-01 18:19:41 -0400

    Update production.py

--- Digicoin/settings/production.py
+++ Digicoin/settings/production.py
@@ -1,5 +1,18 @@
+import os
 from .base import *
 
 DEBUG = False
-# Adicione outras configurações de segurança aqui se desejar
+
+# --- Configurações para Proxy Reverso (Nginx) ---
+# Informa ao Django para confiar nos cabeçalhos enviados pelo proxy.
+USE_X_FORWARDED_HOST = True
+USE_X_FORWARDED_PORT = True
+
+# Ajuda o Django a montar os URLs corretamente quando está em um subdiretório.
+# Ele usará o valor da variável de ambiente DJANGO_SCRIPT_NAME que já configuramos.
+FORCE_SCRIPT_NAME = os.getenv('DJANGO_SCRIPT_NAME')
+
+# Informa ao Django que a conexão é segura (HTTPS) através do proxy.
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 print(">>> Usando configurações de PRODUÇÃO <<<")
